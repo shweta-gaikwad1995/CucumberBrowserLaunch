803a747bc201216291138e8e95c303e63c3070f7
shweta-gaikwad1595 <shwetagaikwad>
2022-01-29 19:44:48 +0530

    commit my Cucumber Html Report

--- src/test/java/StepDefinations/FeatureFileRunner.java
+++ src/test/java/StepDefinations/FeatureFileRunner.java
@@ -7,14 +7,22 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", 
+strict=true,
 glue= {"StepDefinations"},
-monochrome= true,
-plugin = {"pretty", "junit:target/JUnitReports/report.xml",
-		"json:target/JSONReports/report.json",
-		"html:target/HtmlReports",
-		"json:target/cucumber.json"}
+monochrome=true,
+plugin = {"pretty", "junit:target/JUnitReports/report.xml",	//JUnit Report
+		"json:target/JSONReports/report.json",				//JsonReports
+		"html:target/HtmlReports",							//Html report
+		"json:target/cucumber.json",//Cucumber Html Report
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		
+		       }  
+		
+		
+
 
 		)
-public class FeatureFileRunner {
 
+public class FeatureFileRunner {
+	 
 }
--- src/test/java/StepDefinations/GoogleTestRunner.java
+++ src/test/java/StepDefinations/GoogleTestRunner.java
@@ -7,12 +7,15 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features/GoogleSearch.feature", 
-glue= {"StepDefinations"},
+glue= {"StepDefinations"}, //package
 monochrome= true,
 plugin = {"pretty", "junit:target/JUnitReports/report.xml",
 		"json:target/JSONReports/report.json",
 		"html:target/HtmlReports",
-		"json:target/cucumber.json"}
+		"json:target/cucumber.json",
+		"pretty:target/cucmber-pretty.txt",
+		"usage:target/cucumber-usage.json",
+		"junit:target/cucumber-results.xml"}
 
 		)
 public class GoogleTestRunner {
